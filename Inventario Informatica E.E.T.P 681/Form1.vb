d4d154085c1cdf312a814595eac0e105df6a72ea
salan <salan@PC21-SALANORTE>
2025-06-02 13:14:54 -0300

    Cambio modificado

--- Inventario Informatica E.E.T.P 681/Form1.vb
+++ Inventario Informatica E.E.T.P 681/Form1.vb
@@ -6,4 +6,8 @@
     Private Sub Button2_Click(sender As Object, e As EventArgs) Handles Button2.Click
         Me.Close()
     End Sub
+
+    Private Sub kkkkkk_Click(sender As Object, e As EventArgs) Handles kkkkkk.Click
+
+    End Sub
 End Class
