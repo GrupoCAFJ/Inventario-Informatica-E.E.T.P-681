ea0ca7a60dcef4548afa7c2ed30163ab95273ae4
salan <salan@PC22-SALANORTE>
2025-06-30 17:07:30 -0300

    se agrego un data grid viwe

--- Inventario Informatica E.E.T.P 681/Form2.Designer.vb
+++ Inventario Informatica E.E.T.P 681/Form2.Designer.vb
@@ -25,24 +25,31 @@ Partial Class inventariogeneral
         Dim resources As System.ComponentModel.ComponentResourceManager = New System.ComponentModel.ComponentResourceManager(GetType(inventariogeneral))
         Me.Panel1 = New System.Windows.Forms.Panel()
         Me.InventarioG = New System.Windows.Forms.Panel()
+        Me.dgvResultados = New System.Windows.Forms.DataGridView()
         Me.Panel4 = New System.Windows.Forms.Panel()
+        Me.TextBox1 = New System.Windows.Forms.TextBox()
+        Me.Panel6 = New System.Windows.Forms.Panel()
+        Me.Label2 = New System.Windows.Forms.Label()
         Me.PictureBox1 = New System.Windows.Forms.PictureBox()
-        Me.Button2 = New System.Windows.Forms.Button()
         Me.Panel3 = New System.Windows.Forms.Panel()
+        Me.Panel2 = New System.Windows.Forms.Panel()
         Me.Label1 = New System.Windows.Forms.Label()
         Me.Panel5 = New System.Windows.Forms.Panel()
+        Me.Panel7 = New System.Windows.Forms.Panel()
+        Me.Label3 = New System.Windows.Forms.Label()
         Me.PictureBox2 = New System.Windows.Forms.PictureBox()
-        Me.Button3 = New System.Windows.Forms.Button()
         Me.Button1 = New System.Windows.Forms.Button()
-        Me.Panel2 = New System.Windows.Forms.Panel()
         Me.Panel1.SuspendLayout()
         Me.InventarioG.SuspendLayout()
+        CType(Me.dgvResultados, System.ComponentModel.ISupportInitialize).BeginInit()
         Me.Panel4.SuspendLayout()
+        Me.Panel6.SuspendLayout()
         CType(Me.PictureBox1, System.ComponentModel.ISupportInitialize).BeginInit()
         Me.Panel3.SuspendLayout()
-        Me.Panel5.SuspendLayout()
-        CType(Me.PictureBox2, System.ComponentModel.ISupportInitialize).BeginInit()
         Me.Panel2.SuspendLayout()
+        Me.Panel5.SuspendLayout()
+        Me.Panel7.SuspendLayout()
+        CType(Me.PictureBox2, System.ComponentModel.ISupportInitialize).BeginInit()
         Me.SuspendLayout()
         '
         'Panel1
@@ -57,6 +64,7 @@ Partial Class inventariogeneral
         '
         'InventarioG
         '
+        Me.InventarioG.Controls.Add(Me.dgvResultados)
         Me.InventarioG.Controls.Add(Me.Panel4)
         Me.InventarioG.Dock = System.Windows.Forms.DockStyle.Fill
         Me.InventarioG.Location = New System.Drawing.Point(0, 118)
@@ -64,37 +72,62 @@ Partial Class inventariogeneral
         Me.InventarioG.Size = New System.Drawing.Size(800, 332)
         Me.InventarioG.TabIndex = 5
         '
+        'dgvResultados
+        '
+        Me.dgvResultados.ColumnHeadersHeightSizeMode = System.Windows.Forms.DataGridViewColumnHeadersHeightSizeMode.AutoSize
+        Me.dgvResultados.Location = New System.Drawing.Point(351, 6)
+        Me.dgvResultados.Name = "dgvResultados"
+        Me.dgvResultados.Size = New System.Drawing.Size(437, 314)
+        Me.dgvResultados.TabIndex = 5
+        '
         'Panel4
         '
-        Me.Panel4.Controls.Add(Me.PictureBox1)
-        Me.Panel4.Controls.Add(Me.Button2)
+        Me.Panel4.Controls.Add(Me.TextBox1)
+        Me.Panel4.Controls.Add(Me.Panel6)
         Me.Panel4.Location = New System.Drawing.Point(3, 6)
         Me.Panel4.Name = "Panel4"
-        Me.Panel4.Size = New System.Drawing.Size(238, 118)
+        Me.Panel4.Size = New System.Drawing.Size(342, 118)
         Me.Panel4.TabIndex = 4
+        '
+        'TextBox1
+        '
+        Me.TextBox1.BackColor = System.Drawing.SystemColors.Window
+        Me.TextBox1.BorderStyle = System.Windows.Forms.BorderStyle.FixedSingle
+        Me.TextBox1.Font = New System.Drawing.Font("Microsoft Sans Serif", 20.0!)
+        Me.TextBox1.Location = New System.Drawing.Point(145, 41)
+        Me.TextBox1.Name = "TextBox1"
+        Me.TextBox1.Size = New System.Drawing.Size(119, 38)
+        Me.TextBox1.TabIndex = 4
+        '
+        'Panel6
+        '
+        Me.Panel6.Controls.Add(Me.Label2)
+        Me.Panel6.Controls.Add(Me.PictureBox1)
+        Me.Panel6.Location = New System.Drawing.Point(20, 41)
+        Me.Panel6.Name = "Panel6"
+        Me.Panel6.Size = New System.Drawing.Size(119, 37)
+        Me.Panel6.TabIndex = 4
+        '
+        'Label2
+        '
+        Me.Label2.Font = New System.Drawing.Font("Microsoft Sans Serif", 16.0!)
+        Me.Label2.Location = New System.Drawing.Point(0, 0)
+        Me.Label2.Name = "Label2"
+        Me.Label2.Size = New System.Drawing.Size(87, 38)
+        Me.Label2.TabIndex = 5
+        Me.Label2.Text = "Buscar"
+        Me.Label2.TextAlign = System.Drawing.ContentAlignment.MiddleCenter
         '
         'PictureBox1
         '
         Me.PictureBox1.BackColor = System.Drawing.Color.Transparent
         Me.PictureBox1.Image = CType(resources.GetObject("PictureBox1.Image"), System.Drawing.Image)
-        Me.PictureBox1.Location = New System.Drawing.Point(144, 46)
+        Me.PictureBox1.Location = New System.Drawing.Point(83, 0)
         Me.PictureBox1.Name = "PictureBox1"
-        Me.PictureBox1.Size = New System.Drawing.Size(33, 28)
+        Me.PictureBox1.Size = New System.Drawing.Size(36, 38)
         Me.PictureBox1.SizeMode = System.Windows.Forms.PictureBoxSizeMode.Zoom
         Me.PictureBox1.TabIndex = 3
         Me.PictureBox1.TabStop = False
-        '
-        'Button2
-        '
-        Me.Button2.FlatAppearance.BorderSize = 0
-        Me.Button2.FlatStyle = System.Windows.Forms.FlatStyle.Flat
-        Me.Button2.Font = New System.Drawing.Font("Microsoft Sans Serif", 15.75!, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, CType(0, Byte))
-        Me.Button2.Location = New System.Drawing.Point(47, 44)
-        Me.Button2.Name = "Button2"
-        Me.Button2.Size = New System.Drawing.Size(106, 30)
-        Me.Button2.TabIndex = 2
-        Me.Button2.Text = "Buscar"
-        Me.Button2.UseVisualStyleBackColor = True
         '
         'Panel3
         '
@@ -107,6 +140,15 @@ Partial Class inventariogeneral
         Me.Panel3.Size = New System.Drawing.Size(800, 118)
         Me.Panel3.TabIndex = 4
         '
+        'Panel2
+        '
+        Me.Panel2.Controls.Add(Me.Label1)
+        Me.Panel2.Dock = System.Windows.Forms.DockStyle.Left
+        Me.Panel2.Location = New System.Drawing.Point(0, 0)
+        Me.Panel2.Name = "Panel2"
+        Me.Panel2.Size = New System.Drawing.Size(464, 118)
+        Me.Panel2.TabIndex = 6
+        '
         'Label1
         '
         Me.Label1.Font = New System.Drawing.Font("Microsoft Sans Serif", 30.0!)
@@ -118,35 +160,41 @@ Partial Class inventariogeneral
         '
         'Panel5
         '
-        Me.Panel5.Controls.Add(Me.PictureBox2)
-        Me.Panel5.Controls.Add(Me.Button3)
+        Me.Panel5.Controls.Add(Me.Panel7)
         Me.Panel5.Dock = System.Windows.Forms.DockStyle.Right
         Me.Panel5.Location = New System.Drawing.Point(587, 0)
         Me.Panel5.Name = "Panel5"
         Me.Panel5.Size = New System.Drawing.Size(213, 118)
         Me.Panel5.TabIndex = 5
         '
+        'Panel7
+        '
+        Me.Panel7.Controls.Add(Me.Label3)
+        Me.Panel7.Controls.Add(Me.PictureBox2)
+        Me.Panel7.Location = New System.Drawing.Point(13, 38)
+        Me.Panel7.Name = "Panel7"
+        Me.Panel7.Size = New System.Drawing.Size(138, 43)
+        Me.Panel7.TabIndex = 5
+        '
+        'Label3
+        '
+        Me.Label3.Font = New System.Drawing.Font("Microsoft Sans Serif", 20.0!)
+        Me.Label3.Location = New System.Drawing.Point(3, 0)
+        Me.Label3.Name = "Label3"
+        Me.Label3.Size = New System.Drawing.Size(95, 43)
+        Me.Label3.TabIndex = 5
+        Me.Label3.Text = "Volver"
+        Me.Label3.TextAlign = System.Drawing.ContentAlignment.MiddleCenter
+        '
         'PictureBox2
         '
         Me.PictureBox2.Image = CType(resources.GetObject("PictureBox2.Image"), System.Drawing.Image)
-        Me.PictureBox2.Location = New System.Drawing.Point(136, 50)
+        Me.PictureBox2.Location = New System.Drawing.Point(93, 0)
         Me.PictureBox2.Name = "PictureBox2"
-        Me.PictureBox2.Size = New System.Drawing.Size(33, 24)
+        Me.PictureBox2.Size = New System.Drawing.Size(44, 43)
         Me.PictureBox2.SizeMode = System.Windows.Forms.PictureBoxSizeMode.Zoom
         Me.PictureBox2.TabIndex = 4
         Me.PictureBox2.TabStop = False
-        '
-        'Button3
-        '
-        Me.Button3.FlatAppearance.BorderSize = 0
-        Me.Button3.FlatStyle = System.Windows.Forms.FlatStyle.Flat
-        Me.Button3.Font = New System.Drawing.Font("Microsoft Sans Serif", 15.75!, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, CType(0, Byte))
-        Me.Button3.Location = New System.Drawing.Point(44, 44)
-        Me.Button3.Name = "Button3"
-        Me.Button3.Size = New System.Drawing.Size(106, 30)
-        Me.Button3.TabIndex = 3
-        Me.Button3.Text = "Volver"
-        Me.Button3.UseVisualStyleBackColor = True
         '
         'Button1
         '
@@ -163,15 +211,6 @@ Partial Class inventariogeneral
         Me.Button1.Text = "Volver "
         Me.Button1.UseVisualStyleBackColor = True
         '
-        'Panel2
-        '
-        Me.Panel2.Controls.Add(Me.Label1)
-        Me.Panel2.Dock = System.Windows.Forms.DockStyle.Left
-        Me.Panel2.Location = New System.Drawing.Point(0, 0)
-        Me.Panel2.Name = "Panel2"
-        Me.Panel2.Size = New System.Drawing.Size(464, 118)
-        Me.Panel2.TabIndex = 6
-        '
         'inventariogeneral
         '
         Me.AutoScaleDimensions = New System.Drawing.SizeF(6.0!, 13.0!)
@@ -184,25 +223,33 @@ Partial Class inventariogeneral
         Me.Text = "Form2"
         Me.Panel1.ResumeLayout(False)
         Me.InventarioG.ResumeLayout(False)
+        CType(Me.dgvResultados, System.ComponentModel.ISupportInitialize).EndInit()
         Me.Panel4.ResumeLayout(False)
+        Me.Panel4.PerformLayout()
+        Me.Panel6.ResumeLayout(False)
         CType(Me.PictureBox1, System.ComponentModel.ISupportInitialize).EndInit()
         Me.Panel3.ResumeLayout(False)
-        Me.Panel5.ResumeLayout(False)
-        CType(Me.PictureBox2, System.ComponentModel.ISupportInitialize).EndInit()
         Me.Panel2.ResumeLayout(False)
+        Me.Panel5.ResumeLayout(False)
+        Me.Panel7.ResumeLayout(False)
+        CType(Me.PictureBox2, System.ComponentModel.ISupportInitialize).EndInit()
         Me.ResumeLayout(False)
 
     End Sub
     Friend WithEvents Panel1 As Panel
-    Friend WithEvents Button2 As Button
     Friend WithEvents Panel3 As Panel
     Friend WithEvents InventarioG As Panel
     Friend WithEvents Panel5 As Panel
-    Friend WithEvents Button3 As Button
     Friend WithEvents Panel4 As Panel
     Friend WithEvents Button1 As Button
     Friend WithEvents PictureBox1 As PictureBox
     Friend WithEvents PictureBox2 As PictureBox
     Friend WithEvents Label1 As Label
     Friend WithEvents Panel2 As Panel
+    Friend WithEvents TextBox1 As TextBox
+    Friend WithEvents dgvResultados As DataGridView
+    Friend WithEvents Panel6 As Panel
+    Friend WithEvents Label2 As Label
+    Friend WithEvents Panel7 As Panel
+    Friend WithEvents Label3 As Label
 End Class
