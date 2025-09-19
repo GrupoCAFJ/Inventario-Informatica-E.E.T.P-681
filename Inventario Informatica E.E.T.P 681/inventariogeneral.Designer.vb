6f05b366a64f964c9201746a653b5e7d4dc4b3f5
salan <salan@PC22-SALANORTE>
2025-09-19 16:03:52 -0300

    se agregaron filtros

--- Inventario Informatica E.E.T.P 681/inventariogeneral.Designer.vb
+++ Inventario Informatica E.E.T.P 681/inventariogeneral.Designer.vb
@@ -28,6 +28,10 @@ Partial Class inventariogeneral
         Me.Panel9 = New System.Windows.Forms.Panel()
         Me.dgvResultados = New System.Windows.Forms.DataGridView()
         Me.Panel8 = New System.Windows.Forms.Panel()
+        Me.CheckBoxSalaVirgi = New System.Windows.Forms.CheckBox()
+        Me.CheckBoxSum = New System.Windows.Forms.CheckBox()
+        Me.CheckBoxSalaEste = New System.Windows.Forms.CheckBox()
+        Me.CheckBoxSalaNorte = New System.Windows.Forms.CheckBox()
         Me.Panel4 = New System.Windows.Forms.Panel()
         Me.ComboBoxFiltro = New System.Windows.Forms.ComboBox()
         Me.Panel3 = New System.Windows.Forms.Panel()
@@ -35,9 +39,12 @@ Partial Class inventariogeneral
         Me.Label1 = New System.Windows.Forms.Label()
         Me.Panel5 = New System.Windows.Forms.Panel()
         Me.Panel7 = New System.Windows.Forms.Panel()
+        Me.FlechaVolver = New System.Windows.Forms.PictureBox()
         Me.Label3 = New System.Windows.Forms.Label()
-        Me.PictureBox2 = New System.Windows.Forms.PictureBox()
         Me.Button1 = New System.Windows.Forms.Button()
+        Me.Label2 = New System.Windows.Forms.Label()
+        Me.Label4 = New System.Windows.Forms.Label()
+        Me.Panel6 = New System.Windows.Forms.Panel()
         Me.Panel1.SuspendLayout()
         Me.InventarioG.SuspendLayout()
         Me.Panel9.SuspendLayout()
@@ -48,7 +55,8 @@ Partial Class inventariogeneral
         Me.Panel2.SuspendLayout()
         Me.Panel5.SuspendLayout()
         Me.Panel7.SuspendLayout()
-        CType(Me.PictureBox2, System.ComponentModel.ISupportInitialize).BeginInit()
+        CType(Me.FlechaVolver, System.ComponentModel.ISupportInitialize).BeginInit()
+        Me.Panel6.SuspendLayout()
         Me.SuspendLayout()
         '
         'Panel1
@@ -83,7 +91,6 @@ Partial Class inventariogeneral
         'dgvResultados
         '
         Me.dgvResultados.AutoSizeColumnsMode = System.Windows.Forms.DataGridViewAutoSizeColumnsMode.Fill
-        Me.dgvResultados.BorderStyle = System.Windows.Forms.BorderStyle.Fixed3D
         Me.dgvResultados.ColumnHeadersHeightSizeMode = System.Windows.Forms.DataGridViewColumnHeadersHeightSizeMode.AutoSize
         Me.dgvResultados.Dock = System.Windows.Forms.DockStyle.Fill
         Me.dgvResultados.Location = New System.Drawing.Point(0, 0)
@@ -94,6 +101,8 @@ Partial Class inventariogeneral
         '
         'Panel8
         '
+        Me.Panel8.BorderStyle = System.Windows.Forms.BorderStyle.FixedSingle
+        Me.Panel8.Controls.Add(Me.Panel6)
         Me.Panel8.Controls.Add(Me.Panel4)
         Me.Panel8.Dock = System.Windows.Forms.DockStyle.Left
         Me.Panel8.Location = New System.Drawing.Point(0, 0)
@@ -101,21 +110,67 @@ Partial Class inventariogeneral
         Me.Panel8.Size = New System.Drawing.Size(351, 582)
         Me.Panel8.TabIndex = 6
         '
+        'CheckBoxSalaVirgi
+        '
+        Me.CheckBoxSalaVirgi.AutoSize = True
+        Me.CheckBoxSalaVirgi.Font = New System.Drawing.Font("Microsoft Sans Serif", 15.0!)
+        Me.CheckBoxSalaVirgi.Location = New System.Drawing.Point(20, 82)
+        Me.CheckBoxSalaVirgi.Name = "CheckBoxSalaVirgi"
+        Me.CheckBoxSalaVirgi.Size = New System.Drawing.Size(115, 29)
+        Me.CheckBoxSalaVirgi.TabIndex = 8
+        Me.CheckBoxSalaVirgi.Text = "Sala Virgi"
+        Me.CheckBoxSalaVirgi.UseVisualStyleBackColor = True
+        '
+        'CheckBoxSum
+        '
+        Me.CheckBoxSum.AutoSize = True
+        Me.CheckBoxSum.Font = New System.Drawing.Font("Microsoft Sans Serif", 15.0!)
+        Me.CheckBoxSum.Location = New System.Drawing.Point(20, 152)
+        Me.CheckBoxSum.Name = "CheckBoxSum"
+        Me.CheckBoxSum.Size = New System.Drawing.Size(72, 29)
+        Me.CheckBoxSum.TabIndex = 7
+        Me.CheckBoxSum.Text = "Sum"
+        Me.CheckBoxSum.UseVisualStyleBackColor = True
+        '
+        'CheckBoxSalaEste
+        '
+        Me.CheckBoxSalaEste.AutoSize = True
+        Me.CheckBoxSalaEste.Font = New System.Drawing.Font("Microsoft Sans Serif", 15.0!)
+        Me.CheckBoxSalaEste.Location = New System.Drawing.Point(20, 117)
+        Me.CheckBoxSalaEste.Name = "CheckBoxSalaEste"
+        Me.CheckBoxSalaEste.Size = New System.Drawing.Size(115, 29)
+        Me.CheckBoxSalaEste.TabIndex = 6
+        Me.CheckBoxSalaEste.Text = "Sala Este"
+        Me.CheckBoxSalaEste.UseVisualStyleBackColor = True
+        '
+        'CheckBoxSalaNorte
+        '
+        Me.CheckBoxSalaNorte.AutoSize = True
+        Me.CheckBoxSalaNorte.Font = New System.Drawing.Font("Microsoft Sans Serif", 15.0!)
+        Me.CheckBoxSalaNorte.Location = New System.Drawing.Point(20, 47)
+        Me.CheckBoxSalaNorte.Name = "CheckBoxSalaNorte"
+        Me.CheckBoxSalaNorte.Size = New System.Drawing.Size(123, 29)
+        Me.CheckBoxSalaNorte.TabIndex = 5
+        Me.CheckBoxSalaNorte.Text = "Sala Norte"
+        Me.CheckBoxSalaNorte.UseVisualStyleBackColor = True
+        '
         'Panel4
         '
+        Me.Panel4.Controls.Add(Me.Label2)
         Me.Panel4.Controls.Add(Me.ComboBoxFiltro)
         Me.Panel4.Dock = System.Windows.Forms.DockStyle.Top
         Me.Panel4.Location = New System.Drawing.Point(0, 0)
         Me.Panel4.Name = "Panel4"
-        Me.Panel4.Size = New System.Drawing.Size(351, 118)
+        Me.Panel4.Size = New System.Drawing.Size(349, 118)
         Me.Panel4.TabIndex = 4
         '
         'ComboBoxFiltro
         '
+        Me.ComboBoxFiltro.Font = New System.Drawing.Font("Microsoft Sans Serif", 20.0!)
         Me.ComboBoxFiltro.FormattingEnabled = True
-        Me.ComboBoxFiltro.Location = New System.Drawing.Point(56, 53)
+        Me.ComboBoxFiltro.Location = New System.Drawing.Point(101, 40)
         Me.ComboBoxFiltro.Name = "ComboBoxFiltro"
-        Me.ComboBoxFiltro.Size = New System.Drawing.Size(199, 21)
+        Me.ComboBoxFiltro.Size = New System.Drawing.Size(210, 39)
         Me.ComboBoxFiltro.TabIndex = 5
         '
         'Panel3
@@ -131,6 +186,7 @@ Partial Class inventariogeneral
         '
         'Panel2
         '
+        Me.Panel2.BorderStyle = System.Windows.Forms.BorderStyle.FixedSingle
         Me.Panel2.Controls.Add(Me.Label1)
         Me.Panel2.Dock = System.Windows.Forms.DockStyle.Left
         Me.Panel2.Location = New System.Drawing.Point(0, 0)
@@ -141,9 +197,9 @@ Partial Class inventariogeneral
         'Label1
         '
         Me.Label1.Font = New System.Drawing.Font("Microsoft Sans Serif", 30.0!, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, CType(0, Byte))
-        Me.Label1.Location = New System.Drawing.Point(54, 32)
+        Me.Label1.Location = New System.Drawing.Point(69, 32)
         Me.Label1.Name = "Label1"
-        Me.Label1.Size = New System.Drawing.Size(242, 55)
+        Me.Label1.Size = New System.Drawing.Size(212, 55)
         Me.Label1.TabIndex = 6
         Me.Label1.Text = "Inventario"
         '
@@ -158,12 +214,22 @@ Partial Class inventariogeneral
         '
         'Panel7
         '
+        Me.Panel7.Controls.Add(Me.FlechaVolver)
         Me.Panel7.Controls.Add(Me.Label3)
-        Me.Panel7.Controls.Add(Me.PictureBox2)
-        Me.Panel7.Location = New System.Drawing.Point(37, 38)
+        Me.Panel7.Location = New System.Drawing.Point(31, 38)
         Me.Panel7.Name = "Panel7"
-        Me.Panel7.Size = New System.Drawing.Size(138, 43)
+        Me.Panel7.Size = New System.Drawing.Size(150, 43)
         Me.Panel7.TabIndex = 5
+        '
+        'FlechaVolver
+        '
+        Me.FlechaVolver.Image = CType(resources.GetObject("FlechaVolver.Image"), System.Drawing.Image)
+        Me.FlechaVolver.Location = New System.Drawing.Point(96, 0)
+        Me.FlechaVolver.Name = "FlechaVolver"
+        Me.FlechaVolver.Size = New System.Drawing.Size(54, 44)
+        Me.FlechaVolver.SizeMode = System.Windows.Forms.PictureBoxSizeMode.Zoom
+        Me.FlechaVolver.TabIndex = 16
+        Me.FlechaVolver.TabStop = False
         '
         'Label3
         '
@@ -174,16 +240,6 @@ Partial Class inventariogeneral
         Me.Label3.TabIndex = 5
         Me.Label3.Text = "Volver"
         Me.Label3.TextAlign = System.Drawing.ContentAlignment.MiddleCenter
-        '
-        'PictureBox2
-        '
-        Me.PictureBox2.Image = CType(resources.GetObject("PictureBox2.Image"), System.Drawing.Image)
-        Me.PictureBox2.Location = New System.Drawing.Point(93, 0)
-        Me.PictureBox2.Name = "PictureBox2"
-        Me.PictureBox2.Size = New System.Drawing.Size(44, 43)
-        Me.PictureBox2.SizeMode = System.Windows.Forms.PictureBoxSizeMode.Zoom
-        Me.PictureBox2.TabIndex = 4
-        Me.PictureBox2.TabStop = False
         '
         'Button1
         '
@@ -199,6 +255,40 @@ Partial Class inventariogeneral
         Me.Button1.TabIndex = 1
         Me.Button1.Text = "Volver "
         Me.Button1.UseVisualStyleBackColor = True
+        '
+        'Label2
+        '
+        Me.Label2.AutoSize = True
+        Me.Label2.Font = New System.Drawing.Font("Microsoft Sans Serif", 20.0!)
+        Me.Label2.Location = New System.Drawing.Point(39, 43)
+        Me.Label2.Name = "Label2"
+        Me.Label2.Size = New System.Drawing.Size(56, 31)
+        Me.Label2.TabIndex = 6
+        Me.Label2.Text = "Ver"
+        '
+        'Label4
+        '
+        Me.Label4.AutoSize = True
+        Me.Label4.Font = New System.Drawing.Font("Microsoft Sans Serif", 20.0!)
+        Me.Label4.Location = New System.Drawing.Point(11, 13)
+        Me.Label4.Name = "Label4"
+        Me.Label4.Size = New System.Drawing.Size(201, 31)
+        Me.Label4.TabIndex = 9
+        Me.Label4.Text = "Filtrar por salas"
+        '
+        'Panel6
+        '
+        Me.Panel6.BorderStyle = System.Windows.Forms.BorderStyle.FixedSingle
+        Me.Panel6.Controls.Add(Me.Label4)
+        Me.Panel6.Controls.Add(Me.CheckBoxSalaNorte)
+        Me.Panel6.Controls.Add(Me.CheckBoxSalaVirgi)
+        Me.Panel6.Controls.Add(Me.CheckBoxSalaEste)
+        Me.Panel6.Controls.Add(Me.CheckBoxSum)
+        Me.Panel6.Dock = System.Windows.Forms.DockStyle.Fill
+        Me.Panel6.Location = New System.Drawing.Point(0, 118)
+        Me.Panel6.Name = "Panel6"
+        Me.Panel6.Size = New System.Drawing.Size(349, 462)
+        Me.Panel6.TabIndex = 10
         '
         'inventariogeneral
         '
@@ -216,11 +306,14 @@ Partial Class inventariogeneral
         CType(Me.dgvResultados, System.ComponentModel.ISupportInitialize).EndInit()
         Me.Panel8.ResumeLayout(False)
         Me.Panel4.ResumeLayout(False)
+        Me.Panel4.PerformLayout()
         Me.Panel3.ResumeLayout(False)
         Me.Panel2.ResumeLayout(False)
         Me.Panel5.ResumeLayout(False)
         Me.Panel7.ResumeLayout(False)
-        CType(Me.PictureBox2, System.ComponentModel.ISupportInitialize).EndInit()
+        CType(Me.FlechaVolver, System.ComponentModel.ISupportInitialize).EndInit()
+        Me.Panel6.ResumeLayout(False)
+        Me.Panel6.PerformLayout()
         Me.ResumeLayout(False)
 
     End Sub
@@ -230,7 +323,6 @@ Partial Class inventariogeneral
     Friend WithEvents Panel5 As Panel
     Friend WithEvents Panel4 As Panel
     Friend WithEvents Button1 As Button
-    Friend WithEvents PictureBox2 As PictureBox
     Friend WithEvents Label1 As Label
     Friend WithEvents Panel2 As Panel
     Friend WithEvents dgvResultados As DataGridView
@@ -239,4 +331,12 @@ Partial Class inventariogeneral
     Friend WithEvents Panel9 As Panel
     Friend WithEvents Panel8 As Panel
     Friend WithEvents ComboBoxFiltro As ComboBox
+    Friend WithEvents CheckBoxSalaVirgi As CheckBox
+    Friend WithEvents CheckBoxSum As CheckBox
+    Friend WithEvents CheckBoxSalaEste As CheckBox
+    Friend WithEvents CheckBoxSalaNorte As CheckBox
+    Friend WithEvents FlechaVolver As PictureBox
+    Friend WithEvents Label4 As Label
+    Friend WithEvents Label2 As Label
+    Friend WithEvents Panel6 As Panel
 End Class
---
+++ Inventario Informatica E.E.T.P 681/inventariogeneral.vb
@@ -0,0 +1,127 @@
+﻿Imports System.Data.SqlClient
+Imports MySql.Data.MySqlClient
+Public Class inventariogeneral
+
+    'Formlario padre
+    Public Property FormPadre As menuinventario
+    '------------------------------------------------------------------------------------------------------------------------------'
+
+    'Conexcion con la base de datos para la busqueda en la base de datos'
+    ' Definir la cadena de conexión (puedes moverla a app.config)
+    Private connectionString As String = "server=localhost;database=inventarioescuela;user id=root;password=escuela;"
+    Private conexion As MySqlConnection
+
+    Private Sub FormInventario_Load(sender As Object, e As EventArgs) Handles MyBase.Load
+        ' Inicializar la conexión
+        conexion = New MySqlConnection(connectionString)
+
+        ' Llenar el ComboBox con las opciones
+        ComboBoxFiltro.Items.AddRange(New String() {"Todos", "Herramientas", "Arduinos", "Insumos", "Dispositivos", "Periféricos"})
+        ComboBoxFiltro.SelectedIndex = 0
+
+        ' Aplicar filtros iniciales
+        AplicarFiltros()
+    End Sub
+
+    Private Sub AplicarFiltros()
+        Try
+            If conexion.State = ConnectionState.Open Then conexion.Close()
+            conexion.Open()
+
+            Dim query As String = "
+            SELECT m.NOMBRE,
+                   a.NOMBRE AS UBICACION,
+                   s.NOMBRE AS SALA,
+                   ma.CANTIDAD
+            FROM material m
+            JOIN material_almacenamiento ma ON m.ID_MATERIAL = ma.ID_MATERIAL
+            JOIN almacenamiento a ON ma.ID_ALMACENAMIENTO = a.ID_ALMACENAMIENTO
+            JOIN sala s ON a.ID_SALAS = s.ID_SALA
+        "
+
+            ' Lista de filtros
+            Dim filtros As New List(Of String)
+
+            ' Filtro ComboBox
+            Select Case ComboBoxFiltro.SelectedItem.ToString()
+                Case "Herramientas"
+                    filtros.Add("m.TIPO = 'Herramienta'")
+                Case "Arduinos"
+                    filtros.Add("m.TIPO = 'Arduino'")
+                Case "Insumos"
+                    filtros.Add("m.TIPO = 'Insumo'")
+                Case "Dispositivos"
+                    filtros.Add("m.TIPO IN ('Dispositivos','Dispositivo')")
+                Case "Periféricos"
+                    filtros.Add("(m.NOMBRE LIKE '%Monitor%' OR m.NOMBRE LIKE '%Mouse%' OR m.NOMBRE LIKE '%Teclado%')")
+                Case "Todos"
+                    ' No se agrega filtro
+            End Select
+
+            ' Filtro CheckBox
+            Dim salaCondiciones As New List(Of String)
+            If CheckBoxSalaNorte.Checked Then salaCondiciones.Add("s.NOMBRE = 'Sala Norte'")
+            If CheckBoxSalaEste.Checked Then salaCondiciones.Add("s.NOMBRE = 'Sala Este'")
+            If CheckBoxSum.Checked Then salaCondiciones.Add("s.NOMBRE = 'Sum'")
+            If CheckBoxSalaVirgi.Checked Then salaCondiciones.Add("s.NOMBRE = 'Sala Virgi'")
+
+            ' Agregar filtro de salas si hay alguno seleccionado
+            If salaCondiciones.Count > 0 Then filtros.Add("(" & String.Join(" OR ", salaCondiciones) & ")")
+
+            ' Aplicar filtros combinados
+            If filtros.Count > 0 Then
+                query &= " WHERE " & String.Join(" AND ", filtros)
+            End If
+
+            ' Ejecutar consulta y llenar DataGridView
+            Dim da As New MySqlDataAdapter(query, conexion)
+            Dim dt As New DataTable
+            da.Fill(dt)
+            dgvResultados.DataSource = dt
+
+        Catch ex As MySqlException
+            MessageBox.Show("Error al conectar con la base de datos: " & ex.Message)
+        Catch ex As Exception
+            MessageBox.Show("Error: " & ex.Message)
+        Finally
+            If conexion.State = ConnectionState.Open Then conexion.Close()
+        End Try
+    End Sub
+    '------------------------------------------------------------------------------------------------------------------------------'
+
+    'Filtros
+    Private Sub ComboBoxFiltro_SelectedIndexChanged(sender As Object, e As EventArgs) Handles ComboBoxFiltro.SelectedIndexChanged
+        AplicarFiltros()
+    End Sub
+
+    Private Sub CheckBoxSala_CheckedChanged(sender As Object, e As EventArgs) Handles _
+    CheckBoxSalaNorte.CheckedChanged, CheckBoxSalaEste.CheckedChanged, CheckBoxSum.CheckedChanged, CheckBoxSalaVirgi.CheckedChanged
+
+        AplicarFiltros()
+    End Sub
+    '------------------------------------------------------------------------------------------------------------------------------'
+
+    'Boton volver
+    Private Sub Panel7_Click(sender As Object, e As EventArgs) Handles Panel7.Click, Label3.Click, FlechaVolver.Click
+        If FormPadre IsNot Nothing Then
+            FormPadre.MostrarBotones()
+            FormPadre.logoEscuela1.Visible = True
+        End If
+        Me.Close()
+    End Sub
+    Private Sub PanelVolver_MouseEnter(sender As Object, e As EventArgs) Handles Panel7.MouseEnter, Label3.MouseEnter, FlechaVolver.MouseEnter
+        Panel7.BackColor = Color.FromArgb(200, 200, 200) ' Color más oscuro
+    End Sub
+
+    Private Sub PanelVolver_MouseLeave(sender As Object, e As EventArgs) Handles Panel7.MouseLeave, Label3.MouseLeave, FlechaVolver.MouseLeave
+        Panel7.BackColor = Color.White ' Color original '
+    End Sub
+    Private Sub FormInventario_FormClosing(sender As Object, e As FormClosingEventArgs) Handles MyBase.FormClosing
+        ' Liberar recursos de la conexión al cerrar el formulario
+        If conexion IsNot Nothing Then
+            If conexion.State = ConnectionState.Open Then conexion.Close()
+            conexion.Dispose()
+        End If
+    End Sub
+    '------------------------------------------------------------------------------------------------------------------------------'
+End Class
